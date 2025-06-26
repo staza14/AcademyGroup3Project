6cd41b6703878efc65c29ad68a91958bd816e017
Anastasia Florentis <anastasia.florentis@Mac.lan>
2025-06-26 12:02:14 +0100

    automated tests for userstory 6

---
+++ src/test/scala/locators/AddToCartLocators.scala
@@ -0,0 +1,8 @@
+package locators
+
+import org.openqa.selenium.By
+
+object AddToCartLocators {
+  val addToCartButtonsLocator: By = By.className("btn_inventory")
+  val cartNumberIconLocator: By = By.className("shopping_cart_badge")
+}
