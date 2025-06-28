4adf9c9618e80060374ea1df96052f46bf77e5ab
Anastasia Florentis <anastasia.florentis@Mac.lan>
2025-06-28 21:13:38 +0100

    step definitions and locators for userstory 10

--- src/test/scala/stepdefs/ContinueCheckoutSteps.scala
+++ src/test/scala/stepdefs/ContinueCheckoutSteps.scala
@@ -1,8 +1,10 @@
 package stepdefs
 
 import io.cucumber.scala.{EN, ScalaDsl}
+import locators.ContinueCheckoutLocators.{continueCheckoutButtonLocator, continueCheckoutErrorLocator}
 import locators.InitiateCheckoutLocators.{firstNameCheckoutInputLocator, lastNameCheckoutInputLocator, postcodeCheckoutInputLocator}
-import pages.AddToCartPage.inputText
+import pages.AddToCartPage.{clickOn, elementIsPresent, inputText}
+import support.DriverManager.driver
 import testdata.ContinueCheckoutData.{checkoutFirstNameText, checkoutLastNameText, checkoutPostalCodeText}
 
 class ContinueCheckoutSteps extends ScalaDsl with EN {
@@ -16,18 +18,20 @@ class ContinueCheckoutSteps extends ScalaDsl with EN {
     inputText(postcodeCheckoutInputLocator,checkoutPostalCodeText)
   }
   And("""I click the ‘Continue’ button""") { () =>
-    clickOn()
+    clickOn(continueCheckoutButtonLocator)
   }
   Then("""I should be navigated to the Checkout Overview page""") { () =>
-
+    assert(driver.getCurrentUrl == "https://www.saucedemo.com/checkout-step-two.html")
   }
-  When("""I incorrectly fill in the {string}, {string} and {string}""") { () =>
-
+  When("""I incorrectly fill in the {string}, {string} and {string}""") { (firstName: String, lastName: String, postalCode: String) =>
+    inputText(firstNameCheckoutInputLocator,firstName)
+    inputText(lastNameCheckoutInputLocator,lastName)
+    inputText(postcodeCheckoutInputLocator,postalCode)
   }
   Then("""I should remain on the Checkout Information page""") { () =>
-
+    assert(driver.getCurrentUrl == "https://www.saucedemo.com/checkout-step-one.html")
   }
   And("""I should see a checkout error message""") { () =>
-
+    assert(elementIsPresent(continueCheckoutErrorLocator))
   }
 }
